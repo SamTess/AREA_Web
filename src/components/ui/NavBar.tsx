5eb4b6131960ffc3b4e711e0bafd0091afa29c90
marinlamy <marin.lamy@epitech.eu>
2025-09-25 21:00:46 +0200

    [FEAT](navbar): add NavBar component with navigation links and user menu; include styles

---
+++ src/components/ui/NavBar.tsx
@@ -0,0 +1,90 @@
+"use client";
+import { useState } from 'react';
+import {
+  IconGauge,
+  IconHome2,
+  IconLogin,
+  IconListDetails
+} from '@tabler/icons-react';
+import { Center, Stack, Tooltip, UnstyledButton } from '@mantine/core';
+
+import { useRouter } from 'next/navigation';
+import { UserMenu } from './UserMenu';
+import classes from './NavBarMinimal.module.css';
+
+interface NavbarLinkProps {
+  icon: typeof IconHome2;
+  label: string;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+interface UserContent {
+  name: string;
+  email: string;
+  avatarSrc: string;
+}
+
+const user: UserContent = {
+  name: "Test User",
+  email: "testuser@example.com",
+  avatarSrc: "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-7.png"
+};
+
+function NavbarLink({ icon: Icon, label, active, onClick }: NavbarLinkProps) {
+  return (
+    <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+      <UnstyledButton onClick={onClick} className={classes.link} data-active={active || undefined}>
+        <Icon size={20} stroke={1.5} />
+      </UnstyledButton>
+    </Tooltip>
+  );
+}
+
+const dataCenter = [
+  { icon: IconHome2, label: 'Home', link: "/" },
+  { icon: IconGauge, label: 'Dashboard', link: "/dashboard" },
+  { icon: IconListDetails, label: 'Areas', link: "/areas" },
+];
+
+export function NavbarMinimal() {
+  const [active, setActive] = useState(0);
+  const router = useRouter();
+  const [isConnected, setIsConnected] = useState(false);
+
+  const links = dataCenter.map((link, index) => (
+    <NavbarLink
+      {...link}
+      key={link.label}
+      active={index === active}
+      onClick={() => {
+        setActive(index);
+        console.log("Navigating to:", link.link);
+        router.push(link.link);
+      }}
+    />
+  ));
+
+  return (
+    <nav className={classes.navbar}>
+      <Center>
+        <img src="/A1.png" alt="Logo" className={classes.logo}  />
+      </Center>
+
+      <div className={classes.navbarMain}>
+        <Stack justify="center" gap={0}>
+          {links}
+        </Stack>
+      </div>
+
+
+      <Stack justify="center" gap={0}>
+        {isConnected ? (
+          <UserMenu user={user} />
+        ) : (
+          <NavbarLink icon={IconLogin} label="Login" />
+        )}
+      </Stack>
+    </nav>
+  );
+}
