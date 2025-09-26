b7ca657719f1d57077b79e703e87eb9aa93be44e
marinlamy <marin.lamy@epitech.eu>
2025-09-26 15:08:03 +0200

    [FEAT](Authentication): add LoginPage and AuthenticationForm components with password strength validation

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { AuthenticationForm } from '@/components/ui/AuthenticationForm';
+
+export default function LoginPage() {
+  return (
+    <div>
+        <div style={{ height: '130px' }}></div>
+      <AuthenticationForm />
+        <div style={{ height: '130px' }}></div>
+    </div>
+  );
+}
