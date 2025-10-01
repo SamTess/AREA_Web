bf339f515ff824317fcd8de50acb9ea399e32669
marinlamy <marin.lamy@epitech.eu>
2025-10-01 19:42:13 +0200

    [FEAT] (areasService.ts): add action-related functions and mock data handling

--- src/services/areasService.ts
+++ src/services/areasService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { Area, Service } from '../types';
-import { data as mockData, services as mockServices } from '../mocks/areas';
+import { Area, Service, Action } from '../types';
+import { data as mockData, services as mockServices, actions as MockActions, labelsTextReaction1, whiteboardCards} from '../mocks/areas';
 
 const USE_MOCK_DATA = true;
 
@@ -17,3 +17,57 @@ export const getServices = async (): Promise<Service[]> => {
   const response = await axios.get('/api/services');
   return response.data;
 };
+
+export const getServiceById = async (id: number): Promise<Service | undefined> => {
+  if (USE_MOCK_DATA) {
+    const service = mockServices.find(service => service.id === id);
+    return Promise.resolve(service);
+  }
+  const response = await axios.get(`/api/services/${id}`);
+  return response.data;
+}
+
+export const getActionsByServiceId = async (serviceId: number): Promise<Action[]> => {
+  if (USE_MOCK_DATA) {
+    const actions = MockActions.filter(action => action.serviceId === serviceId);
+    return Promise.resolve(actions);
+  }
+  const response = await axios.get(`/api/services/${serviceId}/actions`);
+  return response.data;
+};
+
+export const getActionFieldsByServiceAndActionId = async (serviceId: number, actionId: number): Promise<any[]> => {
+  if (USE_MOCK_DATA)
+    return Promise.resolve(labelsTextReaction1);
+  const response = await axios.get(`/api/services/${serviceId}/actions/${actionId}/fields`);
+  return response.data;
+}
+
+export const createAction = async (action: Partial<Action>): Promise<Action> => {
+  if (USE_MOCK_DATA) {
+    const newAction = { id: Date.now(), ...action } as Action;
+    MockActions.push(newAction);
+    return Promise.resolve(newAction);
+  }
+  const response = await axios.post('/api/actions', action);
+  return response.data;
+}
+
+export const createArea = async (area: Partial<Area>): Promise<Area> => {
+  if (USE_MOCK_DATA) {
+    const newArea = { id: Date.now(), ...area } as Area;
+    mockData.push(newArea);
+    return Promise.resolve(newArea);
+  }
+  const response = await axios.post('/api/areas', area);
+  return response.data;
+}
+
+export const getCardByAreaId = async (areaId: number): Promise<any[]> => {
+  if (!areaId)
+    return Promise.resolve([]);
+  if (USE_MOCK_DATA)
+    return Promise.resolve(whiteboardCards.filter(card => card.areaId === areaId));
+  const response = await axios.get(`/api/areas/${areaId}/cards`);
+  return response.data;
+}
