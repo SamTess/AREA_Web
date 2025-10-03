617117f7b8b2abf3cc13a64ff64be4b92b3a01aa
ctalneau <clement.talneau@epitech.eu>
2025-10-03 14:43:42 +0200

    [FEAT](Deployement): Added necessary route and configuration for deployement

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: process.env.NODE_ENV === 'production' ? 'standalone' : undefined,
   images: {
     remotePatterns: [
       {
@@ -8,6 +9,17 @@ const nextConfig: NextConfig = {
         hostname: 'upload.wikimedia.org',
       },
     ],
+  },
+  turbopack: {
+    root: __dirname,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/client.apk',
+        destination: '/api/client.apk',
+      },
+    ];
   },
 };
 
