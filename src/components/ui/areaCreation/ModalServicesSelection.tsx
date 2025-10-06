35fef8dbb47a729d71f39f8027dd34e02075cabb
marinlamy <marin.lamy@epitech.eu>
2025-10-06 11:20:47 +0200

    [FIX](ModalServicesSelection): add search functionality for services selection

--- src/components/ui/areaCreation/ModalServicesSelection.tsx
+++ src/components/ui/areaCreation/ModalServicesSelection.tsx
@@ -1,10 +1,11 @@
-import { Modal, Button } from '@mantine/core';
+import { Modal, Button, TextInput, Space } from '@mantine/core';
 import { useState, useEffect } from 'react';
 import { getServices } from '../../../services/areasService';
 import { Service, ModalServicesSelectionProps } from '../../../types';
 
 export default function ModalServicesSelection({ isOpen, onClose, onSelect }: ModalServicesSelectionProps) {
     const [services, setServices] = useState<Service[]>([]);
+    const [searchName, setSearchName] = useState<string>('');
 
     useEffect(() => {
         getServices().then((fetchedServices) => {
@@ -13,23 +14,37 @@ export default function ModalServicesSelection({ isOpen, onClose, onSelect }: Mo
     }, []);
 
     return (
-        <Modal opened={isOpen} onClose={onClose} title="Select a Service">
-            {services.map((service) => (
-                <Button
-                    key={service.id}
-                    variant="outline"
-                    fullWidth
-                    mb="sm"
-                    onClick={() => {
-                        onSelect(service);
-                        onClose();
-                    }}
-                    style={{ display: 'flex', alignItems: 'center'}}
-                >
-                    <img src={service.logo} alt={service.name} style={{ width: 20, height: 20 }} />
-                    {service.name}
-                </Button>
-            ))}
+        <Modal opened={isOpen} onClose={onClose} title="Select a Service" size="lg">
+            <TextInput
+                placeholder="Search by name..."
+                value={searchName}
+                onChange={(event) => setSearchName(event.currentTarget.value)}
+            />
+            <Space h="md" />
+            <div style={{ maxHeight: '500px', overflowY: 'auto' }}>
+                {services.filter(service => service.name.toLowerCase().includes(searchName.toLowerCase())).map((service) => (
+                    <div key={service.id} style={{ marginBottom: '8px', paddingRight: '20px', paddingLeft: '20px' }}>
+                    <Button
+                        key={service.id}
+                        variant="outline"
+                        fullWidth
+                        mb="sm"
+                        onClick={() => {
+                            onSelect(service);
+                            onClose();
+                        }}
+                        style={{ display: 'flex', alignItems: 'center' }}
+                    >
+                        <img src={service.logo} alt={service.name} style={{ width: 20, height: 20 }} />
+                        <Space w="8" />
+                        {service.name}
+                    </Button>
+                    </div>
+                ))}
+            </div>
+
+
+
         </Modal>
     );
 }
