9e61a61ce3c237ee7f58437f9d58c6566b778a8e
Samuel Tesson <samuel.tesson@epitech.eu>
2025-10-13 14:26:28 +0200

    [FIX](Tests): Add mock implementations for Mantine charts and update Jest configuration

---
+++ __mocks__/@mantine/charts.js
@@ -0,0 +1,24 @@
+const React = require('react');
+
+const BarChart = ({ data }) => 
+  React.createElement('div', { 'data-testid': 'bar-chart' }, JSON.stringify(data));
+
+const LineChart = ({ data }) => 
+  React.createElement('div', { 'data-testid': 'line-chart' }, JSON.stringify(data));
+
+const AreaChart = ({ data }) => 
+  React.createElement('div', { 'data-testid': 'area-chart' }, JSON.stringify(data));
+
+const PieChart = ({ data }) => 
+  React.createElement('div', { 'data-testid': 'pie-chart' }, JSON.stringify(data));
+
+const DonutChart = ({ data }) => 
+  React.createElement('div', { 'data-testid': 'donut-chart' }, JSON.stringify(data));
+
+module.exports = {
+  BarChart,
+  LineChart,
+  AreaChart,
+  PieChart,
+  DonutChart,
+};
