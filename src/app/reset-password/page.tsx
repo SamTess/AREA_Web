e646dd96dd0136036d9c068703bdac1d8f5ef8ba
Samuel Tesson <samuel.tesson@epitech.eu>
2025-10-14 10:51:05 +0200

    [FEAT](reset-password): Add loading state with Suspense in ResetPasswordPage component

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,14 +1,28 @@
 'use client';
 
+import { Suspense } from 'react';
 import { ResetPasswordForm } from '@/components/ui/auth/ResetPasswordForm';
+import { Loader, Center } from '@mantine/core';
+
+function ResetPasswordContent() {
+  return (
+    <div>
+      <div style={{ height: '130px' }}></div>
+      <ResetPasswordForm />
+      <div style={{ height: '130px' }}></div>
+    </div>
+  );
+}
 
 export default function ResetPasswordPage() {
   return (
-    <div>
-        <div style={{ height: '130px' }}></div>
-      <ResetPasswordForm />
-        <div style={{ height: '130px' }}></div>
-    </div>
+    <Suspense fallback={
+      <Center style={{ height: '100vh' }}>
+        <Loader size="lg" />
+      </Center>
+    }>
+      <ResetPasswordContent />
+    </Suspense>
   );
 }
 
