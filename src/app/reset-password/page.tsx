ea8cc626007a8e700457c13b4a3688322c005fbf
marinlamy <marin.lamy@epitech.eu>
2025-10-14 10:58:37 +0200

    [FEAT](reset-password): wrap ResetPasswordForm in Suspense for improved loading state

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import { Suspense } from 'react';
 import { ResetPasswordForm } from '@/components/ui/auth/ResetPasswordForm';
 
 export default function ResetPasswordPage() {
   return (
     <div>
         <div style={{ height: '130px' }}></div>
-      <ResetPasswordForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ResetPasswordForm />
+      </Suspense>
         <div style={{ height: '130px' }}></div>
     </div>
   );
